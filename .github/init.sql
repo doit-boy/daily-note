a02609145d912d42f817c516caf07ceecf54370c
dv <1114145341@qq.com>
2023-11-08 09:47:45 +0800

    增加数据表

--- .github/init.sql
+++ .github/init.sql
@@ -1,28 +1,3 @@
-# ************************************************************
-# Sequel Ace SQL dump
-# 版本号： 20046
-#
-# https://sequel-ace.com/
-# https://github.com/Sequel-Ace/Sequel-Ace
-#
-# 主机: prod.material.host (MySQL 8.0.33)
-# 数据库: daily-note
-# 生成时间: 2023-06-25 05:27:51 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-SET NAMES utf8mb4;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE='NO_AUTO_VALUE_ON_ZERO', SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# 转储表 task
-# ------------------------------------------------------------
-
 CREATE TABLE `task` (
     `id` bigint unsigned NOT NULL AUTO_INCREMENT,
     `user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
@@ -36,19 +11,9 @@ CREATE TABLE `task` (
     KEY `INDEX_USER_ID` (`user_id`,`sort`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-LOCK TABLES `task` WRITE;
-/*!40000 ALTER TABLE `task` DISABLE KEYS */;
-
 INSERT INTO `task` (`id`, `user_id`, `name`, `summary`, `is_deleted`, `created_at`, `updated_at`)
 VALUES
 	(1,1,'单测','专门用于单元测试',0,'2023-06-25 13:24:16','2023-06-25 13:24:48');
-
-/*!40000 ALTER TABLE `task` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-# 转储表 task_item
-# ------------------------------------------------------------
 
 CREATE TABLE `task_item` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
@@ -63,11 +28,6 @@ CREATE TABLE `task_item` (
   UNIQUE KEY `UNIQUE_VALUE` (`task_id`,`date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
-
-# 转储表 user
-# ------------------------------------------------------------
-
 CREATE TABLE `user` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `openid` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'OpenID',
@@ -77,21 +37,32 @@ CREATE TABLE `user` (
   UNIQUE KEY `UNIQUE_OPENID` (`openid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-
 INSERT INTO `user` (`id`, `openid`, `created_at`, `updated_at`)
 VALUES
 	(1,'ohjUY0TB_onjcaH2ia06HgGOC4CY','2023-06-25 12:37:08','2023-06-25 12:37:08');
 
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `ys_player` (
+ `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+ `user_id` bigint unsigned NOT NULL DEFAULT '0' COMMENT '用户 ID',
+ `uid` bigint unsigned NOT NULL DEFAULT '0' COMMENT '原神 UID',
+ `comment` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '备注',
+ `is_deleted` tinyint unsigned NOT NULL DEFAULT '0' COMMENT '是否删除',
+ `created_at` datetime NOT NULL DEFAULT '2023-01-01 00:00:00',
+ `updated_at` datetime NOT NULL DEFAULT '2023-01-01 00:00:00',
+ PRIMARY KEY (`id`),
+ UNIQUE KEY `UNIQUE_UID` (`user_id`,`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE `ys_roler` (
+`id` bigint unsigned NOT NULL AUTO_INCREMENT,
+`user_id` bigint NOT NULL DEFAULT '0' COMMENT '用户 ID',
+`uid` bigint NOT NULL DEFAULT '0' COMMENT '原神 UID',
+`role` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '角色名',
+`role_img` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '角色头像',
+`level` int unsigned NOT NULL DEFAULT '0' COMMENT '角色等级',
+`role_data` json DEFAULT NULL COMMENT '原始数据',
+`created_at` datetime NOT NULL DEFAULT '2023-01-01 00:00:00',
+`updated_at` datetime NOT NULL DEFAULT '2023-01-01 00:00:00',
+PRIMARY KEY (`id`),
+UNIQUE KEY `UNIQUE_UID` (`user_id`,`uid`,`role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
